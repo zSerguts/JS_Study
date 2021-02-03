c5e0b4f61e49449d475d48ec208f86d1df80847d
Zhenya Serguts <z.serguts@gmail.com>
2021-02-03 23:55:27 +0300

    Lesson4 commit

--- script.js
+++ script.js
@@ -1,53 +1,25 @@
 'use strict';
 
-//1
-const lang = prompt('Выберите язык');
-const week = [
-    ["Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье."], 
-    ["Monday, Tuesday, Wednesday, Thursday, Friday, Sunday, Saturday."]
-];
-const miss = "На таком языке мы не говорим.";
-
-//a
-if (lang === "ru"){
-    console.log (week[0]);
-} 
-else if (lang === "en"){
-    console.log (week[1]);
-}
-else {
-    console.log(miss);
+function outputMessage(name){
+    console.log('Hello ' + name);
+    console.log('Hello ' + name);
 }
 
+outputMessage('Max');
 
-//b
-switch(lang){
-    case "ru":
-        console.log (week[0]);
-        break;
-    case "en":
-        console.log (week[1]);
-        break;
-    default:
-        console.log(miss);
+// const consoleMessage = function(){
+//     console.log('Hello, friend!')
+// }
+
+// consoleMessage();
+
+// const alertMessage = new Function ('alert("Hi!")');
+
+// alertMessage();
+
+const sum = function(a, b){
+    let res = a + b;
+    console.log(res);
 }
 
-//через тернарку
-
-console.log(lang === "ru" ? week[0] : lang === "en" ? week[1] : miss);
-
-
-//c
-
-const data = [];
-
-data.ru = ['Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье.'];
-data.en = ['Monday, Tuesday, Wednesday, Thursday, Friday, Sunday, Saturday.'];
-
-console.log(String(data[lang]));
-
-
-//2
-
-const namePerson = prompt('Введите имя:');
-console.log (namePerson === 'Артём' ? "директор" : namePerson === 'Максим' ? "преподаватель" : "такого человека нет.");
+sum(3,5);
