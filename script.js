6def58bb0dbb9d48fb254ef6ecb94e6f8dd2cc3a
Zhenya Serguts <z.serguts@gmail.com>
2021-02-03 22:49:30 +0300

    Lesson2 commit

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-var money = 123;
-var income, addExpenses, deposit, mission, period;
-alert('Это моя домашка к первому уроку.');
+let money = 500;
+let income = ' Frilance';
+let addExpenses = 'Internet, taxi, commynalka';
+let deposit = true;
+let mission = 5000;
+let period = 12;
+
+
+console.log (money, income, deposit);
+console.log (addExpenses.length);
+console.log ("Период равен", period, "месяцев. Цель заработать", mission, "рублей" );
+
+addExpenses.toLowerCase();
+let arr = [];
+for ( let i = 0; i < addExpenses.length; i++)
+{
+    arr[i] = addExpenses[i];
+}
+console.log (arr);
+
+let budgetDay = money / 30;
+console.log (budgetDay, "В день");
