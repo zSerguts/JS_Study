03693c5a95b045c8ee69cc538d40fd0e3922ca96
Zhenya Serguts <z.serguts@gmail.com>
2021-02-06 18:49:41 +0300

    fixed(script.js): Исправил на константы, доделал ТЗ.

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-let money = 500;
-let income = ' Frilance';
-let addExpenses = 'Internet, taxi, commynalka';
-let deposit = true;
-let mission = 5000;
-let period = 12;
+const money = 500,
+    income = ' Frilance',
+    addExpenses = 'Internet, taXi, coMMynalka',
+    deposit = true,
+    mission = 5000,
+    period = 12;
 
 
-console.log (money, income, deposit);
+console.log(typeof(money), typeof(income), typeof(deposit));
 console.log (addExpenses.length);
 console.log ("Период равен", period, "месяцев. Цель заработать", mission, "рублей" );
 
-addExpenses.toLowerCase();
-let arr = [];
-for ( let i = 0; i < addExpenses.length; i++)
-{
-    arr[i] = addExpenses[i];
-}
-console.log (arr);
+
+console.log (addExpenses.toLowerCase().split());
 
 let budgetDay = money / 30;
 console.log (budgetDay, "В день");
